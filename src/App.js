d2167913712073a2f73038bf51bd15c605fa352c
Wellington Suero <144365343+Wellfc@users.noreply.github.com>
2024-05-29 14:19:52 -0500

    Created React Intro Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// App is the main component
+/*
+  Browsers can't 'read' JSX code.
+
+  Conventions:
+    - All elements must be closed.
+    - Empty elements (br, hr, img, input...) must be closed with a '/>' at the end.
+    - JSX uses camelCase style (onClick, tabIndex, className...).
+    - Use curly braces to embed JavaScript expressions in JSX.
+    - Use double curly braces to embed JavaScript objects in JSX.
+*/
+
+// Spoiler: this is a hook. We'll learn about them later.
+import { useState } from 'react'; // useState is between curly braces because it's a named export.
 
 function App() {
+  const [userName, setUserName] = useState(''); // userName is the state variable, setUserName is the function to update it.
+  // const [userAge, setUserAge] = useState(0); // useState(0) initializes the state variable with 0.
+
+  const handleInput = event => {
+    setUserName(event.target.value);
+  }
+
+
+  // let title = 'Intro to React';
+  let message = <h2>Welcome to React, <span>{userName}</span></h2>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        {message}
+        
+        <input
+          type='text'
+          className='input'
+          placeholder='User name'
+          onChange={handleInput}
+        />
+
+      </div>
+    </main>
   );
 }
 
